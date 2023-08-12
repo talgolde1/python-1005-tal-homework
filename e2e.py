4a64d03d136aa6a2da42f6ef0504477ed428a060
talgolde1 <100038455+talgolde1@users.noreply.github.com>
2023-08-12 14:11:29 +0300

    webdriver update

--- e2e.py
+++ e2e.py
@@ -1,14 +1,21 @@
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_scores_service(app_url):
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(app_url)
-    # Locate and check the score element
-    score_element = driver.find_element("score")
-    score = int(score_element.text)
-    is_valid_score = 1 <= score <= 1000
+    try:
+        wait = WebDriverWait(driver, 10)
+        score_element = wait.until(EC.presence_of_element_located((By.ID, "score")))
+        score = int(score_element.text)
+        is_valid_score = 1 <= score <= 1000
+    except Exception as e:
+        print("An error occurred:", e)
+        is_valid_score = False
     driver.quit()
     return is_valid_score
 
@@ -24,4 +31,3 @@ if __name__ == "__main__":
     app_url = "http://localhost:8777/"
     exit_code = main_function(app_url)
     exit(exit_code)
-
